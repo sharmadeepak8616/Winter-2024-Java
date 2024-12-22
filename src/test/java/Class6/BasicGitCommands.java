3ee4af7e9dc1d3fe015417cc64398350657a1b72
Deepak Sharma <sharma.deepak8616@gmail.com>
2024-12-22 12:31:09 -0500

    Basic command

--- src/test/java/Class6/BasicGitCommands.java
+++ src/test/java/Class6/BasicGitCommands.java
@@ -11,8 +11,11 @@ public class BasicGitCommands {
          *      command to add files/folder to be sent to remote
          *
          * git commit
+         *      command to write a short-message to mention why are we sending the files to remote
+         *      git commit -m "short message"
          *
          * git push
+         *      command to actually sent files (with commit message) in the remote
          *
          */
     }
